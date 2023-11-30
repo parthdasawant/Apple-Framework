514e2378f74ce49e16c11e0bbd00b05974b00b4e
parthdasawant <parthdasawant07@gmail.com>
2023-11-30 17:40:40 +0530

    Initial Commit

---
+++ Apple Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple Framework
+//
+//  Created by Dnyanesh on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
